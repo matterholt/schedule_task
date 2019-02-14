85354cc0672375310d304d0fbb9dd601281243b1
MAtterholt <matterholt84@gmail.com>
2019-02-13 23:22:49 -0500

    imporved code by using new object

---
+++ README.md
@@ -0,0 +1,12 @@
+# Task Scheduler
+
+    End goal is to take all task and make a svg to simulate the fetch style
+
+## todo list
+
+    Create a clear button
+    Button for completed task, and add date, change color like grey out.
+    Add a total task count, task left to complete, and how many have been completed.
+        - organized by next one due
+        - move completed class to the end.
+    Change the days left to the task list
--- main.js
+++ main.js
@@ -1,67 +1,71 @@
+
+const taskListGlobal = []
+
 // Button to get data and start the script
 const buttonGetData = document.getElementById('displayValue')
-buttonGetData.addEventListener('click', getData )
+buttonGetData.addEventListener('click', getData)
 
-//current Date -- NEED to be refreshed daily
+//  current Date -- NEED to be refreshed daily
 const todayIs = new Date()
-document.getElementById('dateCurrent').innerHTML = todayIs.toDateString();
+document.getElementById('dateCurrent').innerHTML = todayIs.toDateString()
 
-//covert date to array of numbers
-function dateConvert (date){
-    const dates = date[1]
-    const splitDate = dates.map(x => x.split('-'));
-    const dateFormat = splitDate.map(x => new Date(x));
-    return dateFormat;
+//  covert date to array of numbers
+function dateConvert (date) {
+  const splitDate = date.split('-')
+  const dateFormat = new Date(splitDate)
+  return dateFormat
 }
 // calculate the dat left
-function dayLeft (date) {
-    const msPerDay = 24*60*60*1000;
-    const dayCal = (date[1].getTime() - todayIs.getTime()) / msPerDay;
-    const dayLeft = Math.round(dayCal);
-    if (dayLeft == 1) {
-        const dayLeftDisplay = `${dayLeft} DAY LEFT !!!`
-        document.getElementById("daysLeftOut").innerHTML = dayLeftDisplay
-        document.getElementById('daysLeftOut').style.color = "LightCoral";
-    } else if (dayLeft > 1){
-        const dayLeftDisplay = `${dayLeft} days left `
-        document.getElementById("daysLeftOut").innerHTML = dayLeftDisplay
-        document.getElementById('daysLeftOut').style.color = "DarkBlue";
-    }else{
-        const dayLeftDisplay = `LAST day get it together ! `
-        document.getElementById("daysLeftOut").innerHTML = dayLeftDisplay
-        document.getElementById('daysLeftOut').style.color = "red";
-    }
+function dayLeft (dateEnd) {
+  const msPerDay = 24 * 60 * 60 * 1000
+  const dayCal = ((dateEnd.getTime() - todayIs.getTime()) / msPerDay)
+  const dayLeft = Math.round(dayCal)
+  if (dayLeft === 1) {
+    const dayLeftDisplay = `${dayLeft} DAY LEFT !!!`
+    document.getElementById('daysLeftOut').innerHTML = dayLeftDisplay
+    document.getElementById('daysLeftOut').style.color = 'LightCoral'
+  } else if (dayLeft > 1) {
+    const dayLeftDisplay = `${dayLeft} days left `
+    document.getElementById('daysLeftOut').innerHTML = dayLeftDisplay
+    document.getElementById('daysLeftOut').style.color = 'DarkBlue'
+  } else {
+    const dayLeftDisplay = `LAST day! get it done ! `
+    document.getElementById('daysLeftOut').innerHTML = dayLeftDisplay
+    document.getElementById('daysLeftOut').style.color = "'red'"
+  }
 }
 
-//WORKING and not used
-function addToList (currentTaskData) {
-    const y_task = currcurrentTaskData[0]
-    const twoDate = currcurrentTaskData[1]
-    const startDate = twoDate[0]
-    const completedDate = twoDate[1]
-    // add data to a list in the HTML
-    const ul = document.querySelector("ul")
-    const objective = (`The ${y_task} will start ${startDate} and hope to projected to completed ${completedDate}`)
-    const li =  document.createElement("li");
-    ul.appendChild(li).append(objective)
+function addToListHTML (dateTask) {
+  // add data to a list in the HTML
+  const ul = document.querySelector('ol')
+  const objective = (`${dateTask.task} will start ${dateTask.startDate} and projected to completed ${dateTask.endDate}`)
+  const li = document.createElement('li')
+  ul.appendChild(li).append(objective)
 }
 
-// Grab Data from HTML 
-function getData() {
-    // array of dates
-    const keyDate = []
-    const currentTaskData = []
-    // DOM interactions
-    const y_task = document.getElementById('currentTask').value
-    const startDate = document.getElementById('dateStart').value
-    const completedDate = document.getElementById('dateComplete').value
+function TaskObj (task, startDate, endDate) {
+  this.task = task
+  this.startDate = startDate
+  this.endDate = endDate
+}
 
-    // add Dates to an array
-    keyDate.push(startDate,completedDate)
-    // add task and date array to an array
-    currentTaskData.push(y_task, keyDate )
+// Grab Data from HTML
+function getData () {
+  // DOM interactions
+  const yTask = document.getElementById('currentTask').value
+  const startDate = document.getElementById('dateStart').value
+  const completedDate = document.getElementById('dateComplete').value
 
-    // Calculate the date left in Task
-    const datesConvert = dateConvert(currentTaskData);
-    const dayLeftForTask = dayLeft(datesConvert)
+  const createTask = new TaskObj(yTask, startDate, completedDate)
+  console.log(createTask)
+  // Should create a class?? study to see the difference
+
+  // push to global array, maybe use it for some data analysis
+  taskListGlobal.push(createTask)
+  addToListHTML(createTask)
+
+  // convert the end date to a date object
+  const dateEndConvert = dateConvert(createTask.endDate)
+  dayLeft(dateEndConvert)
+  console.log(`This is the end date ${dateEndConvert} `)
 }
